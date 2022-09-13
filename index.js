5a757125114cab720b7cf4c23fada5b592341579
iNite Gaming <62105998+AI-nsley69@users.noreply.github.com>
2022-09-13 22:07:52 +0000

    Move files to api dir & add .gitignore

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const app = express();
-const espers = require("./espers.js");
+const espers = require("./api/espers.js");
 let sheet;
 
 const PORT = 443;
 app.listen(PORT, async () => {
-  console.log(`Listening on port ${PORT}`);
-  sheet = await espers.prepareDoc();
-  console.log("Finished loading sheet");
+    console.log(`Listening on port ${PORT}`);
+    sheet = await espers.prepareDoc();
+    console.log("Finished loading sheet");
 })
 
 app.get("/", (req, res) => {
-  res.send("Available endpoints: /dislyte/esper/$ESPER_NAME")
+    res.send("Available endpoints: /dislyte/esper/$ESPER_NAME")
 })
 
 app.get("/dislyte/esper/:esper", async (req, res) => {
-  try {
-    const esper = req.params.esper;
-    let esperInfo;
-    esperInfo = await espers.getData(esper, sheet)
-    res.json(esperInfo);
-  } catch (err) {
-    console.log(err);
-    res.status(404);
-    res.json({ error: err.toString() });
-  }
+    try {
+        const esper = req.params.esper;
+        let esperInfo;
+        esperInfo = await espers.getData(esper, sheet)
+        res.json(esperInfo);
+    } catch (err) {
+        console.log(err);
+        res.status(404);
+        res.json({ error: err.toString() });
+    }
 })
